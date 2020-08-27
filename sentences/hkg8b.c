8073f3686bddb12ea1d1174fbc8975c386ee2a91
Huzaifa Khan <hkg8b@umsystem.edu>
2020-08-27 10:15:03 -0500

    commiting again

--- sentences/hkg8b.c
+++ sentences/hkg8b.c
@@ -2,10 +2,10 @@
 
 // Function to include & executeA
 int hkg8bSentence1() {
-    printf("and the abyss stared back.\n");
+    printf("He stared at the cat.\n");
     return 1;
 }
 int hkg8bSentence_FINAL() {
-    printf("and realized he was the cat all along.\n");
+    printf("The two cats were NOT the same cats.\n");
     return 1;
 }
