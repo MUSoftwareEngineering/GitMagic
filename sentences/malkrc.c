c1a34f9f92a9e0f2c60a5778b68f3c73d93102c3
malkrc <malkrc@mail.missouri.edu>
2020-08-27 14:09:48 -0500

    Created malkrc.c with placeholder text

---
+++ sentences/malkrc.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+  
+
+// Function to include & execute
+int malkrcSentence1()
+{
+  printf("\nMalkrc Sentence 1. \n");
+  return 1;
+}
+
+int malkrcSentence2()
+{
+  printf("\nMalkrc Sentence 2. \n");
+  return 1;
+}
