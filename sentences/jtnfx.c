aff2f254537d7355e7b1622df90204d7a41314cb
Jingsong Tan <jtnfx@mail.missouri.edu>
2020-08-27 14:51:36 -0500

    story change

--- sentences/jtnfx.c
+++ sentences/jtnfx.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int jtnfxSentence1() {
-    printf("jtnfx1");
+    printf("The cat wandered around around, but he couldn't find a mouse anywhere.\n");
     return 1;
 }
 
 int jtnfxSentence2() {
-    printf("jtnfx2");
+    printf("He is so hungry, and he turned his eye to Superman.\n");
     return 1;
 }
