d409175417f965d442c2d206218010a4d5026158
Mike Weiss <mjwcbc@umsystem.edu>
2020-08-28 16:31:39 -0500

    Create mjwcbc.c

---
+++ sentences/mjwcbc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int mjwcbcSentence1() {
+    printf("Superman left without hesitation.\n");
+    return 1;
+}
+
+int mjwcbcSentence2() {
+    printf("The boy wondered why this had happened.\n");
+    return 1;
+}
