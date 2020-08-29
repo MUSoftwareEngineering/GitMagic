6e9883234900155f98cd583088c72062822911b4
Joseph <pikminzeldamarioguy@gmail.com>
2020-08-29 18:53:24 -0500

    Create jlmygh.c

---
+++ sentences/jlmygh.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int jlmygh1(){
+
+    printf("He had a random migraine");
+    return 0;
+}
+
+int jlmygh2(){
+
+    printf("There were tic-tacs at the counter");
+    return 0;
+}
