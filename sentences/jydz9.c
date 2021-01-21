39571dfcecd26d9e740d7aebb2d88b7f647e5e60
Jim Yang <76235469+Celestialz2@users.noreply.github.com>
2021-01-21 17:39:14 -0600

    added two sentences

---
+++ sentences/jydz9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//two print functions
+
+int jydz9Sentence1()
+{
+	printf("Bill the frog and Henry didn't notice the cat.");
+	return 1;
+}
+
+int jydz9Sentence2()
+{
+	printf("As they were too busy staring at each other ready for a fight");
+	return 1;
+}
