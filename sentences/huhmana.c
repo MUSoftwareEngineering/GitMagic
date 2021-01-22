849e688b8d7dbd40f833f24e7bfdb72c64debf35
Andrew <huhmana@umsystem.edu>
2021-01-22 14:50:41 -0600

    Added my sentences

---
+++ sentences/huhmana.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int huhmanaSentence1()
+{
+	printf("The dog wiped its eyes");
+	return 1;
+}
+
+int huhmanaSentence_Final()
+{
+	printf("It wasn't actually a bird, it was superman.");
+	return 1;
+}
