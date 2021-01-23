1019fd55d4e568b4a2de82fc8fff5ab75d84ea03
chloegjones <chloegrcjones@gmail.com>
2021-01-23 17:48:37 -0600

    added sentences to cgjnd7.c

---
+++ sentences/cgjnd7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int cgjnd71(){
+    printf("Then, Bill made some cereal.\n");
+}
+
+int cgjnd72(){
+    printf("Just kidding, the frong prince was made up. Frogs aren't princes! Henry was actually a pickle.\n");
+}
