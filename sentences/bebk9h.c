dd97c9e5e4a08b11768ddb3f7896e06ce4a2171b
Ben Bradshaw <bebk9h@umsystem.edu>
2021-01-22 18:44:03 -0600

    Fixed bebk9h.c

--- sentences/bebk9h.c
+++ sentences/bebk9h.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int bebk9hSentence1(){
-	printf(\n"Leonard was down but not out.\n");
+	printf("\nLeonard was down but not out.\n");
 	return 1;
 }
 int bebk9hSentence2(){
-	printf(\n"With the last of his strength, he rose from the ground\n");
+	printf("\nWith the last of his strength, he rose from the ground\n");
 	return 1;
 }
 
