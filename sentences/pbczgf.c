d510ccc723c82267c40ed94fdbcfe84dba457e26
Alex LeDuc <ajleduc7@gmail.com>
2021-01-25 23:55:10 -0600

    Re-added new student files manually b/c I cant figure out how to do it automatically.

---
+++ sentences/pbczgf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int pbczgfSentence1() {
+    printf("It became clear to the boy that he must be suffering a psychotic break, as none of the previous events made any rational sense.\n");
+    return 0;
+}
+
+
+int pbczgfSentence2() {
+    printf("Henry felt extremely gratified in his actions; he knew that all cats, no matter their appearance, are the embodiment of pure evil and should be eradicated.\n");
+    return 0;
+}
---
+++ sentences/sdwfkd.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+// Function to include & execute
+int sdwfkdSentence1()
+{
+  printf("\nThen suddenly the skies parted and the sun shined down on Henry.\n");
+  return 1;
+}
+
+int sdwfkdSentence2()
+{
+  printf("\nHe began to weep.\n");
+  return 1;
+}
