6275e7c5be88ab256afc6a8550592c02b79a1bcf
jjtncv9 <78004013+jjtncv9@users.noreply.github.com>
2021-01-25 21:11:29 -0600

    Create jjtncv.c

---
+++ sentences/jjtncv.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int jjtncvSentence1()
+{
+  printf("But his internal hatred wasn't enough for Henry, Henry needed to act on his thoughts.\n");
+  return 1;
+}
+
+int jjtncvSentence2()
+{
+  printf("However, his malice couldn't overshadow the fact that he secretly wished them a happy afterlife in
+  heaven, or wherever it may be.\n");
+  return 1;
+}
