db5c438bdda9fcf211fbaf969ae6fee3520b061d
Samuel McIntyre <sjmc5n@umsystem.edu>
2021-01-25 21:47:47 -0600

    fixing a return error

--- sentences/cgjnd7.c
+++ sentences/cgjnd7.c
@@ -3,9 +3,10 @@
 
 int cgjnd71(){
     printf("Then, Bill made some cereal.\n");
+    return 1;
 }
 
 int cgjnd72(){
     printf("Just kidding, the frong prince was made up. Frogs aren't princes! Henry was actually a pickle.\n");
+    return 1;
 }
-
