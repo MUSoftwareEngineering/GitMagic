cbab80fa29f2aba1c50ddbf037859c18231451f1
Sean P. Goggins <s@goggins.com>
2021-08-24 13:15:06 -0500

    initial

--- story.c
+++ story.c
@@ -3,26 +3,33 @@
 
 // I assume all of you know C.
 
+int storySentence1()
+{
+  printf("Once upon a time\n");
+  return 1;
+}
+
+int storySentence2()
+{
+  printf("\nThe end. \n");
+  return 1;
+}
+
+
 int main( void )
 {
 	printf("\n\n");
-	bensSentence1();
+	storySentence1();
 	printf("\n\n");
 
 	// Add your sentence.
 	// Don't forget to update the header file.
 	// vvvvvvvvvvvvvvvv STUDENT WORK HERE
-	printf("\n\n");
-	lhn95sen1();
-	printf("\n\n");
-	lhn95sen2();
-	printf("\n\n");
-	    
 	    
 	    
 	// DO NOT ADD AFTER THIS COMMENT
 	// Bens sentence should be last.
-	bensSentence_FINAL();
+	storySentence2();
 	printf("\n\n");
 }
 
