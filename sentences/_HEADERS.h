9d549ddc0a583a12b2581b11372f9e62a19d9d1a
David McCullah <dwmxn8@umsystem.edu>
2021-09-01 16:06:48 -0500

    Formatted function names.

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -5,15 +5,15 @@ int bpbkt7Sentence2();
 
 // DECLARE YOUR symbols here, implement them in a standalone C file
 // vvv STUDENT WORK vvv
-int tmgnnwSentence();
+int tmgnnwSentence1();
 int tmgnnwSentence2();
-int storySentence3();
-int storySentence4();
+int sldn3fSentence1();
+int sldn3fSentence2();
 int nrb8qfSentence1();
 int nrb8qfSentence2();
 int kafgc2Sentence1();
 int kafgc2Sentence2();
-int kgk2btSentenceOne();
-int kgk2btSentenceTwo();
+int kgk2btSentence1();
+int kgk2btSentence2();
 int dwmxn8Sentence1();
 int dwmxn8Sentence2();
