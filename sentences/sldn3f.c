3512299e7a853b49d0b114037a7b3ed60c380a2d
David McCullah <dwmxn8@umsystem.edu>
2021-09-01 15:59:21 -0500

    Formatted spacing.

--- sentences/sldn3f.c
+++ sentences/sldn3f.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int sldn3fSentence1() {
-printf("I walked into Lafferre\n");
-return 1;
+    printf("I walked into Lafferre\n");
+    return 1;
 }
 
 int sldn3fsentence2() {
-printf("And fell down the stairs\n");
-return 1;
+    printf("And fell down the stairs\n");
+    return 1;
 }
