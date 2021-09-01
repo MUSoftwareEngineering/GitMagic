e52f1f29b476d2043792f405d158cefd844e4349
David McCullah <dwmxn8@umsystem.edu>
2021-09-01 16:01:50 -0500

    Formatted spacing.

--- sentences/kafgc2.c
+++ sentences/kafgc2.c
@@ -1,16 +1,11 @@
-# include <stdio.h>
+#include <stdio.h>
 
-
-// Function to include & execute
-int kafgc2Sentence1()
-{
+int kafgc2Sentence1() {
   printf("in order to get to my CS4450 lecture\n");
   return 1;
 }
 
-int kafgc2Sentence2()
-{
+int kafgc2Sentence2() {
   printf("but it looks like Noah figured it out.\n");
   return 1;
 }
-
