1b876fbdc0b5d10efd2e9225635dcc9e915e7f94
Johnson Luu <jlz83@umsystem.edu>
2021-09-02 19:29:29 -0500

    Compiled and fixed spelling on Manas PR.

--- sentences/mkcbd.c
+++ sentences/mkcbd.c
@@ -2,12 +2,12 @@
 
 int ManasSentence1()
 {
-  printf("The King used his powers to slay the serpent\n");
+  printf("The King used his powers to slay the serpent.\n");
   return 1;
 }
 
 int ManasSentence_FINAL()
 {
-  printf("\nThe King loved his new dog\n");
+  printf("The King loved his new dog.\n");
   return 1;
 }
