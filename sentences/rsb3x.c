94d9c19ac13039ca8b9455ef8641920f9aafc3ed
Romelo-S <89549904+Romelo-S@users.noreply.github.com>
2021-09-04 18:45:18 -0500

    Update rsb3x.c

--- sentences/rsb3x.c
+++ sentences/rsb3x.c
@@ -1,11 +1,11 @@
 # include<stdio.h>
 int rsb3xSentence1()
 {
-  printf("Not only was it just a lake of juice but a lake full of cereal fish");
+  printf("Not only was it just a lake of juice but a lake full of cereal fish.\n");
   return 1;
 }
 int rsb3xSentence2()
 {
-  printf("The cereal fish were all Frosted Flakes.");
+  printf("The cereal fish were all Frosted Flakes.\n");
   return 1;
 }
