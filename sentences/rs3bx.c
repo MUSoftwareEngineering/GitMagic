009806b2b2d9abd0b9cb1571fdc39f688600b416
Romelo-S <89549904+Romelo-S@users.noreply.github.com>
2021-09-04 18:16:36 -0500

    Update rs3bx.c
    Baisc code too hard

--- sentences/rs3bx.c
+++ sentences/rs3bx.c
@@ -1,2 +1,11 @@
-Not only was it just a lake of juice but a lake full of cereal fish
-The cereal fish were all Frosted Flakes.
+# include<st.dio>
+int rsb3xSentence1()
+{
+  printf("Not only was it just a lake of juice but a lake full of cereal fish");
+  return 1;
+}
+int rsb3xSentence2()
+{
+  printf("The cereal fish were all Frosted Flakes.");
+  return 1;
+}
