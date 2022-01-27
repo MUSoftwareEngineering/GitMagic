b5db4f7a84cd602404a9a83d5cb528636484109f
S3nnah39 <tsofyx@mail.missouri.edu>
2022-01-27 13:04:36 -0600

    Added sentences to my file

---
+++ sentences/tsofyx.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int tashaSentence1(){
+    printf("Smoke covered the sky.\n ");
+    return 1;
+}
+
+int tashaSentence2(){
+    printf("\n The embers painted the ground black.\n");
+    return 1;
+}
