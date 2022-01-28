e0b7bee079185b21ba32b1c5daca24bfa1599402
k1nty <k1nty@protonmail.com>
2022-01-28 17:46:15 -0600

    new story commit

---
+++ sentences/kjkvfw.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int kjkvfwSentence1() 
+{
+	printf("While the girdle bought our fated hero's some time, the dragon was right on their tail.\n");
+     	return 1;
+}
+
+int kjkvfwSentence2() 
+{
+	printf("Suddenly, the princess trips and falls.\n");
+        return 1;
+}
