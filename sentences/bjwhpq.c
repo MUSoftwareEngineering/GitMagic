a734eb182ec28735f6f8fb9fd00c343e936d0465
winningham <54338413+winningham@users.noreply.github.com>
2022-01-29 00:34:53 -0600

    add new sentences source

---
+++ sentences/bjwhpq.c
@@ -0,0 +1,17 @@
+//bjwhpq sentences
+
+# include <stdio.h>
+
+int bjwhpqSentence1()
+{
+  printf("So they all made hot chocolate and s'mores.");
+  return 1;
+}
+
+int bjwhpqSentence2()
+{
+  printf("It strikes the dragon on the head with surprising force for such a small object.\n");
+  return 1;
+}
+
+
