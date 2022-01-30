b0a6720927d70f6b035668dda1a2288347d93caa
JoelAHilliard <hilliardjoel05@gmail.com>
2022-01-29 22:44:43 -0600

    return1 not 0

--- sentences/jahm6v.c
+++ sentences/jahm6v.c
@@ -3,11 +3,11 @@
 int jahm6vSentence1()
 {
   printf("\nThe Rams beat the 49ers 34-31.\n");
-  return 0;
+  return 1;
 }
 
 int jahm6vSentence2()
 {
   printf("Aaron Donald with 4 sacks.\n");
-  return 0;
+  return 1;
 }
