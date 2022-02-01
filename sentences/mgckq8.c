7d24cf20da5148807147adc6023b32ecce5ed3c8
Melissa Crozier <mgckq8@mail.missouri.edu>
2022-01-31 22:05:23 -0600

    Updated mgckq8 sentences

--- sentences/mgckq8.c
+++ sentences/mgckq8.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int mgckq8Sentence1() {
-    printf("Their long battle was exhausting, but they were still high on adrenaline.");
+    printf("Their long battle was exhausting, but they were still high on adrenaline.\n");
     return 1;
 }
 
 int mgckq8Sentence2() {
-    printf("At night they sat around the campfire and practiced witchcraft.");
+    printf("At night they sat around the campfire and practiced witchcraft.\n");
     return 1;
 }
