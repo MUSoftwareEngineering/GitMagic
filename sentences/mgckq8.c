2e9e1ed1f9dcada5a9abcef194ef63c54a92547b
Melissa Crozier <mgckq8@mail.missouri.edu>
2022-01-31 21:54:42 -0600

    Added mgckq8 sentences

---
+++ sentences/mgckq8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int mgckq8Sentence1() {
+    printf("Their long battle was exhausting, but they were still high on adrenaline.");
+    return 1;
+}
+
+int mgckq8Sentence2() {
+    printf("At night they sat around the campfire and practiced witchcraft.");
+    return 1;
+}
