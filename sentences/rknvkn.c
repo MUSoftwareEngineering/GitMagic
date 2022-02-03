51030b51c07aa6842425bded3bb02f4b5a22fc22
RajuNichols <93800462+RajuNichols@users.noreply.github.com>
2022-02-02 19:15:16 -0600

    Create rknvkn.c
    added sentences

---
+++ sentences/rknvkn.c
@@ -0,0 +1,12 @@
+//Contributed by Raju Nichols (rknvkn)
+#include <stdio.h>
+
+int rknvknSentence1(){
+    printf("Goggins, knowing she had passed out took her to the local physician.\n");
+    return 1;
+}
+
+int rknvknSentence2(){
+    printf("The local physician, Roland Oruche told Goggins that there was a special procedure but it was very risky.\n");
+    return 1;
+}
